c09a76ef30e700cadf6e5c70f6e02ff865dad629
Guilherme Vieira <guivpw68@gmail.com>
2021-08-08 23:56:46 -0300

    feat: add name field validation

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -57,4 +57,48 @@ describe('Sign Up', () => {
     cy.get('li')
       .should('contain', 'Some Name - some@email.com - core - git-it')
   })
+
+  it('Displays error and disable submit button if name is not given', () => {
+    cy.visit('/')
+
+    cy.get('input[name="email"]')
+      .click()
+      .type('some@email.com')
+      .should('have.value', 'some@email.com')
+
+    cy.get('select[name="department"]')
+      .select('core')
+      .should('have.value', 'core')
+    
+    cy.get('img[alt="loading"]')
+      .should('exist')
+      .and('have.attr', 'src', '/img/loading.gif')
+
+    cy.get('select[name="course"]')
+      .select('git-it')
+      .should('have.value', 'git-it')
+
+    cy.get('input[name="name"]')
+      .click()
+      .type(' ')
+      .should('have.value', ' ')
+      .clear()
+      .should('have.value', '')
+    
+    cy.get('span')
+      .eq(0)
+      .should('contain', 'Name Required')
+      .and('have.attr', 'style', 'color: red;')
+    
+    cy.get('input[type="submit"]')
+      .should('be.disabled')
+    
+    cy.get('input[name="name"]')
+      .click()
+      .type('Some name')
+      .should('have.value', 'Some name')
+    
+    cy.get('input[type="submit"]')
+      .should('not.be.disabled')
+  })
 })
