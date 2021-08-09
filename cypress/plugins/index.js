72abc60b9a65d9412077ab4d126fe8d6541055f4
Guilherme Vieira <guivpw68@gmail.com>
2021-08-09 03:42:57 -0300

    test: add react plugins

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+const injectDevServer = require('@cypress/react/plugins/react-scripts')
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -17,6 +19,6 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+	injectDevServer(on, config)
+	return config
 }
