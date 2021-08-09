56d54e1166745f925a2cbb80d24ba70aa31b626a
Guilherme Vieira <guivpw68@gmail.com>
2021-08-08 23:47:40 -0300

    fix: flaky await test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -2,6 +2,11 @@ describe('Sign Up', () => {
   it('Adds person to course', () => {
     cy.visit('/')
 
+    cy.get('h1')
+      .should('contain', 'Sign Up Sheet')
+
+    cy.get('input[type="submit"').should('be.disabled')
+
     cy.get('input[name="name"]')
       .click()
       .type('Some Name')
@@ -10,18 +15,45 @@ describe('Sign Up', () => {
     cy.get('input[name="email"]')
       .click()
       .type('some@email.com')
-
+      .should('have.value', 'some@email.com')
+      
     cy.get('select[name="department"]')
       .select('core')
       .should('have.value', 'core')
+  
+    cy.get('select[name="course"]')
+      .should('not.exist')
+    
+    cy.get('h3')
+      .should('contain', 'People')
+    
+    cy.get('img[alt="loading"]')
+      .should('exist')
+      .and('have.attr', 'src', '/img/loading.gif')
+    
+    cy.get('select[name="course"]')
+      .should('exist')
 
     cy.get('select[name="course"]')
       .select('git-it')
       .should('have.value', 'git-it')
+    
+    cy.get('img[alt="loading"]')
+      .should('not.exist')
 
+    cy.get('span')
+      .should('have.value', '')
+    
     cy.get('input[type="submit"]')
+      .should('not.be.disabled')
       .click()
-
+      .should('be.disabled')
+      .and('have.value', 'Saving...')
+    
+    cy.get('input[type="submit"]', { timeout: 4500 } )
+      .should('have.value', 'Saved!')
+      .and('be.disabled')
+    
     cy.get('li')
       .should('contain', 'Some Name - some@email.com - core - git-it')
   })
