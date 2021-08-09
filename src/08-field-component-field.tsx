e11b0dfd84aca21d4861cef57653d75fed192015
Guilherme Vieira <guivpw68@gmail.com>
2021-08-09 00:40:31 -0300

    refactor: use typescript and react hooks

---
+++ src/08-field-component-field.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+interface FieldProps {
+	name: string
+	value: string
+	placeholder?: string
+	validate: (value: string) => boolean
+	onChange: ({ name, value, error }) => void
+}
+
+interface FieldState {
+	value?: string
+	error?: boolean
+}
+
+export const Field = ({
+	name,
+	value: defaultValue,
+	placeholder,
+	onChange,
+	validate
+}: FieldProps) => {
+	const [state, setState] = useState<FieldState>({
+		value: defaultValue,
+		error: false
+	})
+
+	const handleChangeField = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value
+		const error = validate ? validate(value) : false
+
+		setState({
+			value,
+			error
+		})
+
+		onChange({ name, value, error })
+	}
+
+	return (
+		<div>
+			<input
+				name={name}
+				placeholder={placeholder}
+				value={state.value}
+				onChange={handleChangeField}
+			/>
+			<span style={{ color: 'red' }}>{state.error}</span>
+		</div>
+	)
+}
